ed83e2518a76e356272db1c463aeac8d57acd0a6
wangyu19810820 <gadfly0036_cn@sina.com>
2018-09-06 08:39:35 +0800

    aa

--- logbackdemo/src/main/java/demo/Demo1.java
+++ logbackdemo/src/main/java/demo/Demo1.java
@@ -10,11 +10,11 @@ public class Demo1 {
     public static void main(String[] args) {
         Logger logger = LoggerFactory.getLogger(Demo1.class);
         for(int i = 0; i < 1; i++) {
-            logger.error("Hello world.Demo5");
+            logger.error("Hello world.Demo6");
 //            logger.warn("Hello world.Demo1");
 //            logger.info("Hello world.Demo1");
 //            logger.debug("Hello world.Demo1");
-//            logger.trace("Hello world.Demo1");
+            logger.trace("Hello world.Demo1");
         }
 
         LoggerContext lc = (LoggerContext)LoggerFactory.getILoggerFactory();
